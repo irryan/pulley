cc53f3ec06daf1fbcb02baab0795a1ea9069bcc9
Ian Ryan <ian.ryan@gmail.com>
2024-03-22 14:22:51 -0400

    Reached level 6; SHA-256 hash.

--- app.py
+++ app.py
@@ -17,6 +17,14 @@ def rotate_string(input: str, d: int) -> str:
 def xor(data: bytes, key: bytes) -> bytes:
     return bytearray(d^k for d, k in zip(data, cycle(key)))
 
+def unscramble(data: bytes, key: bytes) -> str:
+    print(data)
+    print(key)
+    dec = [0] * 32
+    for d, k in zip(data, key[3:]):
+        dec[k] = d
+    return ''.join(dec)
+
 def find_encryption_method(method: str) -> str:
     match method:
         case "nothing": return lambda enc: enc
@@ -24,13 +32,13 @@ def find_encryption_method(method: str) -> str:
         case "inserted some non-hex characters": return lambda enc: re.sub(nonhex_regex, "", enc)
 
     circular_rotation_regex = r"circularly rotated (\w+) by (\d+)"
-    if re.match(circular_rotation_regex, method):
-        offset = re.match(circular_rotation_regex, method)[2]
+    if m := re.match(circular_rotation_regex, method):
+        offset = m[2]
         return lambda enc: rotate_string(enc, int(offset))
 
     xor_encryption_regex = r"hex decoded, encrypted with XOR, hex encoded again. key: (\w+)"
-    if re.match(xor_encryption_regex, method):
-        key = re.match(xor_encryption_regex, method)[1].encode()
+    if m := re.match(xor_encryption_regex, method):
+        key = m[1].encode()
         print(key)
         def decoder(enc):
             hex = binascii.unhexlify(enc)
@@ -41,11 +49,10 @@ def find_encryption_method(method: str) -> str:
         return decoder
 
     scrambled_regex = r"scrambled! original positions as base64 encoded messagepack: (.*+)"
-    if re.match(scrambled_regex, method):
-        pos = re.match(scrambled_regex, method)[1]
-        print(pos)
-        dec_pos = base64.b64decode(pos)
-        print(dec_pos)
+    if m := re.match(scrambled_regex, method):
+        pos = m[1]
+        key = base64.b64decode(pos)
+        return lambda enc: unscramble(enc, key)
 
     raise Exception(f"Method ({method}) not found")
 
@@ -63,9 +70,8 @@ def main():
         encryption_method = find_encryption_method(json_obj["encryption_method"])
         task_id = encryption_method(enc_task_id)
         
-        print(task_id)
         req_path = f"task_{task_id}"
-        print(req_path)
 
 if __name__ == "__main__":
     main()
+
